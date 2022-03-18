6bbf9759d03bb9776e362032987987efa0e8f6ca
LeeanPR95 <64715058+LeeanPR95@users.noreply.github.com>
2022-03-17 21:33:24 -0300

    Correciones menores
    Correciones de diseño y nueva animación en el título.

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,15 @@
 
-
 // Acá podemos cambiar el titulo con una variable en js
-const miTitulo = document.querySelector(".Mititulo")
-miTitulo.textContent = 'Blog de LeeanPR95';
+/*const miTitulo = document.querySelector(".Mititulo")
+miTitulo.textContent = ('Blog de LeeanPR95'); 
+*/
 
 const yourname = document.querySelector('#yourname');
-yourname.onclick = function() {
+yourname.onclick = function () {
     let name = prompt("Dime tu nombre, adelante UwU");
     alert(`Hola ${name}, bienvenido a esta humilde página OwO`)
 }
 
-
-document.querySelector('.tocame').onclick = function()
-{
+document.querySelector('.tocame').onclick = function () {
     alert("Hmmm... ¿Dónde estás tocando? (⁄ ⁄>⁄ ▽ ⁄<⁄ ⁄)");
 }
